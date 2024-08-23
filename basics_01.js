e9738083424e72ee4895fb72ab21ea782b9c36e7
Safi Hyder <safihaider0987@gmail.com>
2024-08-23 16:49:44 +0530

    Refactor basics_01.js to include comments and explanations for primitive and reference data types

--- basics_01.js
+++ basics_01.js
@@ -12,7 +12,25 @@
 // let negValue=-value;
 // console.log(negValue);
 
-let value1="safi"
-let value2=" Hyder"
-let value3=value1+value2;
-console.log(value3);
+// let value1="safi"
+// let value2=" Hyder"
+// let value3=value1+value2;
+// console.log(value3);
+////Primitive Data Types
+//7 types of primitive data types
+//String ,Number ,Boolean ,Null ,Undefined ,Symbol ,BigInt
+
+//Symbol and BigInt are relatively new data types in JavaScript. They were introduced in ECMAScript 2015 (ES6). The Symbol data type is used to create unique identifiers for objects, while BigInt is used to represent integers that are larger than the maximum number that can be represented by the Number data type.
+let id=Symbol("123");
+let anotherId=Symbol("123");
+console.log(id===anotherId); //false
+const bigNumber=BigInt(12345678901234567890);
+console.log(bigNumber);
+
+
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime. Unlike languages such as Typescript or Java, you don't need to declare the data type of a variable explicitly.
+
+
+//Reference(non_primitive) Data Types
+//Array,Objects,Functions
+//The datatype of non primitive datatypes is object
