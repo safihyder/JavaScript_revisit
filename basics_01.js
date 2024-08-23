995b2c6759f7fb477d177825a08ea092db62f8c8
Safi Hyder <safihaider0987@gmail.com>
2024-08-23 16:32:31 +0530

    basics

---
+++ basics_01.js
@@ -0,0 +1,18 @@
+// let score="sfsf";
+// // console.log(typeof score)
+// //conversion from number to string
+// // let scoreAsString=String(score);
+// // console.log(typeof scoreAsString); 
+// // the value if it is the not number and ,we convert it in number then it will return NaN and type will show number
+// let scoreAsString=Number(score);
+// console.log(typeof scoreAsString); 
+// console.log(scoreAsString); 
+////////////////////OPERATIONS////////////////////////
+// let value=3
+// let negValue=-value;
+// console.log(negValue);
+
+let value1="safi"
+let value2=" Hyder"
+let value3=value1+value2;
+console.log(value3);
